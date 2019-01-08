95cc89303a8a24192539f1c34a53669323b11e8a
diaaanek <dkorongy@gmail.com>
2019-01-08 14:42:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,33 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
+  # sends a 200 status code
+  # has a link with the text "List a Puppy"
+  # has a link to list a puppy that links to /new
+
+  get '/' do
+      erb :index
+    end
+
+
+    # get '/index' do
+    #     erb :create_puppy
+    #   end
+
+      post '/new' do
+          erb :create_puppy
+        end
+
+    # sends a 200 status code
+    # renders a form that can POST a name, breed, and age
+
+      # sends a 200 status code
+      #  recieves a request from /new at /puppy
+      #  displays the puppy information dynamically
+      post '/puppy' do
+          @puppy = Puppy.new(params['name'], params['breed'],params['age'])
+          erb :display_puppy
+        end
+
 end
