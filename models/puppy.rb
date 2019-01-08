f77261bf3d2addf8ef710ecaf5ebc6b7a68521ac
Nison Malayev <nmalayev@gmail.com>
2019-01-08 14:10:36 -0500

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,19 +1,24 @@
 class Puppy
 
   attr_accessor :name, :breed, :age
+  # attr_reader :age
 
   @@all = []
-
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-    @@all << self
-  end
 
   def self.all
     @@all
   end
+
+  def initialize(params)
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    @@all << self
+  end
+
+  # def age=(age)
+  #   @age = params[:age]
+  # end
 
 
 
