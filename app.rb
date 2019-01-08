063ccb9be1fb8a46e5f126cf40ab413e0cc3024b
github username <github email address>
2019-01-08 11:47:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+#get request to home page , link to index
   get '/' do
   erb :index
   end
@@ -10,12 +11,14 @@ class App < Sinatra::Base
   # erb :create_puppy
   # end
 
+#set up controller to view form
   get '/new' do
-  erb :create_puppy
+  erb :create_puppy #form is in this file
   end
 
+#third controller action that takes user input and renders view for new puppy
   post '/puppy' do
   @puppy = Puppy.new(params['name'],params['breed'], params['age'])
-  erb :display_puppy
+  erb :display_puppy # displays puppy to user , makes results dynamic ie: @puppy.name
 end
 end
